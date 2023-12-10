a34a79d00a2cec0115acc86d452c1e8711e52374
jovansurya <90240775+Jovansurya@users.noreply.github.com>
2023-12-10 15:30:53 +0700

    Update Constants.kt
    ubah url

--- Rasa android/app/src/main/java/com/shashank/rasabot/util/Constants.kt
+++ Rasa android/app/src/main/java/com/shashank/rasabot/util/Constants.kt
@@ -3,7 +3,7 @@ package com.shashank.rasabot.util
 class Constants {
 
     companion object {
-        val NGROCK_URL = "https://c4f6-103-94-190-20.ngrok.io"
+        val NGROCK_URL = "https://dfa3-35-223-13-126.ngrok-free.app"
         val BASE_URL = "$NGROCK_URL/webhooks/rest/"
         val NETWORK_TIMEOUT = 5000L
         val MESSAGE_TEXT_NULL = "NA"
